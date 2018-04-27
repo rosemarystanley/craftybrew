314a06d47eb45b5196fbe4a2aeda876553ef0c1d
Ken Stanley <dohpaz@gmail.com>
2018-04-27 11:07:19 -0400

    Render the HTML list of map markers

--- app/Resources/javascript/main.js
+++ app/Resources/javascript/main.js
@@ -20,23 +20,45 @@ fetch('/api/breweries')
     return response.json()
   })
   .then(function (breweries) {
+    const mapList = document
+      .getElementById('map-list')
+        .getElementsByTagName('ul')[0];
+
     breweries.forEach(function (brewery) {
       const marker = L
         .marker([brewery.latitude, brewery.longitude])
         .addTo(map);
 
+      const h5 = document.createElement('h5');
+            h5.appendChild(document.createTextNode(brewery.name));
+
+      const dt = document.createElement('dt');
+            dt.appendChild(document.createTextNode('Address'));
+
+      const dd = document.createElement('dd');
+            dd.appendChild(document.createTextNode(brewery.address));
+            dd.appendChild(document.createElement('br'));
+            dd.appendChild(document.createTextNode(
+              brewery.city + ', ' +
+              brewery.state + ' ' +
+              brewery.postal)
+            );
+
+      const dl = document.createElement('dl');
+            dl.appendChild(dt);
+            dl.appendChild(dd);
+
+      const listItem = document.createElement('li');
+            listItem.appendChild(h5);
+            listItem.appendChild(dl);
+            listItem.className = 'list-group-item';
+
+      mapList.appendChild(listItem);
+
       // TODO Templating
       marker.bindPopup(
-        '<h5>' + brewery.name + '</h5>' +
-        '<dl>' +
-        '  <dt>Address</dt>' +
-        ' <dd>' +
-            brewery.address + '<br>' +
-            brewery.city + ', ' +
-            brewery.state + ' ' +
-            brewery.postal +
-        ' </dd>' +
-        '</dl>'
+        h5.outerHTML +
+        dl.outerHTML
       );
     });
 
