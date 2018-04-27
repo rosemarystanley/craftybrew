4a9063930d982cf1ce28aa5cabc1ab3195727723
Ken Stanley <dohpaz@gmail.com>
2018-04-27 11:53:39 -0400

    Add scrolling animation when selecting a marker

--- app/Resources/javascript/main.js
+++ app/Resources/javascript/main.js
@@ -64,6 +64,7 @@ fetch('/api/breweries')
         });
 
         listItem.classList.add('active');
+        scrollTo(mapList, listItem.offsetTop, 1250)
       }.bind(null, mapList, listItem));
 
       // TODO Templating
@@ -76,3 +77,51 @@ fetch('/api/breweries')
   })
   .catch(function (error) {});
 
+/**
+ * @see https://gist.github.com/andjosh/6764939
+ *
+ * @param element
+ * @param to
+ * @param duration
+ */
+function scrollTo(element, to, duration) {
+  const start = element.scrollTop;
+  const change = to - start;
+  const increment = 20;
+
+  let currentTime = 0;
+
+  const animateScroll = function() {
+    currentTime += increment;
+
+    element.scrollTop = easeInOutQuad(currentTime, start, change, duration);
+
+    if (currentTime < duration) {
+      setTimeout(animateScroll, increment);
+    }
+  };
+
+  animateScroll();
+}
+
+/**
+ * @see https://gist.github.com/andjosh/6764939
+ *
+ * @param t current time
+ * @param b start value
+ * @param c change in value
+ * @param d duration
+ *
+ * @returns {*}
+ */
+const easeInOutQuad = function (t, b, c, d) {
+  t /= d/2;
+
+  if (t < 1) {
+    return c / 2 * t * t + b;
+  }
+
+  t--;
+
+  return -c / 2 * (t * (t - 2) - 1) + b;
+};
