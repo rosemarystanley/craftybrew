37356557e16f86f4ae7e53219568664bb53303d1
Ken Stanley <dohpaz@gmail.com>
2018-05-03 15:59:57 -0400

    Add user initial location to the map
    If the user blocks location, then default to the center of Charlotte, NC.

--- app/Resources/javascript/components/Locations/Map.js
+++ app/Resources/javascript/components/Locations/Map.js
@@ -8,17 +8,51 @@ class LocationsMap extends React.Component {
 
     this.handleAccessTokenChange = this.handleAccessTokenChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleLocationFound = this.handleLocationFound.bind(this);
+    this.handleLocationError = this.handleLocationError.bind(this);
 
     this.state = {
-      latlng: [35.227, -80.843], // Charlotte, NC, USA
+      latlng: [0, 0],
       zoom: 13,
       maxZoom: 18,
       style: 'mapbox.light',
+      origin: [0, 0],
+      originColor: '#883333',
     };
+  }
+
+  componentDidMount() {
+    this.refs.map.leafletElement.locate();
   }
 
   handleLocationChange(e) {
     this.props.onLocationsChange(e.target.value);
+  }
+
+  handleLocationFound(e) {
+    const map = this.refs.map.leafletElement;
+    map.stopLocate();
+
+    console.log('Location found: %o', e);
+
+    this.state.origin = e.latlng;
+
+    L.marker(this.state.origin).addTo(map).bindPopup('You are here.').openPopup();
+    L.circle(this.state.origin, {radius: e.accuracy / 2 * this.state.zoom, color: this.state.originColor}).addTo(map);
+    map.flyTo(this.state.origin, this.state.zoom);
+  }
+
+  handleLocationError(e) {
+    const map = this.refs.map.leafletElement;
+    map.stopLocate();
+
+    console.log('Unable to get location: %o', e);
+
+    this.state.origin = [35.227, -80.843];
+
+    L.marker(this.state.origin).addTo(map).bindPopup('You are here.').openPopup();
+    L.circle(this.state.origin, {radius: 26.2 * this.state.zoom, color: this.state.originColor}).addTo(map);
+    map.flyTo(this.state.origin, this.state.zoom);
   }
 
   handleAccessTokenChange(e) {
@@ -52,7 +86,14 @@ class LocationsMap extends React.Component {
     }
 
     return (
-      <Map center={latlng} zoom={zoom} id="map">
+      <Map
+        center={latlng}
+        zoom={zoom}
+        id="map"
+        ref="map"
+        onLocationfound={this.handleLocationFound}
+        onLocationError={this.handleLocationError }
+      >
         <TileLayer
           attribution="Map data &copy; <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a> contributors, <a href=&quot;http://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;http://mapbox.com&quot;>Mapbox</a>"
           url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}"
